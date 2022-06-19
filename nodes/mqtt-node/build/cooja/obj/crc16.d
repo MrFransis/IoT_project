e12ae90b98a08c8193f5af171af92f61095b606f
MrFransis <franceh96@hotmail.it>
2022-06-19 16:31:30 -0400

    -

--- nodes/mqtt-node/build/cooja/obj/crc16.d
+++
@@ -1,2 +0,0 @@
-build/cooja/obj/crc16.o: ../../../../os/lib/crc16.c
-../../../../os/lib/crc16.c :
--- nodes/mqtt-node/build/cooja/obj/random.d
+++
@@ -1,4 +0,0 @@
-build/cooja/obj/random.o: ../../../../os/lib/random.c \
- ../../../../os/lib/random.h
-../../../../os/lib/random.c :
- ../../../../os/lib/random.h :
